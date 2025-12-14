63dd31814637ab27d0c975de021b3fdbf8b53b8d
athallahdx <athallahgamer4@gmail.com>
2025-12-14 19:22:46 +0700

    Refactor AuthRepository: remove unused imports to clean up the code

--- lib/app/data/repositories/user_repository.dart
+++ lib/app/data/repositories/user_repository.dart
@@ -1,10 +1,3 @@
-import 'dart:convert';
-import 'dart:io';
-import 'package:http/http.dart' as http;
-import 'package:gym_app/app/data/models/user.dart';
-import 'package:gym_app/app/data/providers/api_routes.dart';
-import 'package:gym_app/app/core/services/user_info.dart';
-
 class AuthRepository {
   // This class appears to be empty or incomplete
   // Methods should be added here
